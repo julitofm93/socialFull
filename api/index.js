c89bd0b3d5c3bba9726405ee6e1b4c0177a4f2d0
julitofm93 <julian.fm@live.com>
2023-02-03 18:02:41 -0300

    Auth login & register

--- api/index.js
+++ api/index.js
@@ -4,9 +4,21 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
-
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+//middlewares
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next();
+})
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3000",
+}))
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
